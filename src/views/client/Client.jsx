9700a7b4309167d2b644771884239849a225893f
Jeff <visoskyj@oregonstate.edu>
2020-08-02 13:52:34 +0300

    question prompt functionality; category legend

--- src/views/client/Client.jsx
+++ src/views/client/Client.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 import Board from  '../Gameboard/Gameboard'
 import Gamelogic from  '../../components/Gamelogic/Gamelogic'
 
+import RollagainSquare from '../../components/Squares/RollagainSquare'
+import PeopleSquare from '../../components/Squares/PeopleSquare'
+import PlaceSquare from '../../components/Squares/PlaceSquare'
+import EventSquare from '../../components/Squares/EventSquare'
+import HolidaySquare from '../../components/Squares/HolidaySquare'
+import HubSquare from '../../components/Squares/HubSquare'
 
 
 class Client extends React.Component {
@@ -29,10 +35,25 @@ class Client extends React.Component {
         console.info("Client: the configuration given is: " + response);
     }
 
+    questionPrompt(question){
+        let user_answer = prompt("Category is " + this.currentCategory + 
+            "\n\n" + question["question"] + "\n\n" + 
+            "1. " + question["answers"][0] + "\n\n" +
+            "2. " + question["answers"][1] + "\n\n" +
+            "3. " + question["answers"][2] + "\n\n", 
+            "Give number of answer.");
+
+        // let correct_answer = question["correct_answer"]
+        let correct_answer = '1'
+        if(user_answer == correct_answer)
+            return 'correct'
+        else return 'incorrect'
+    }
+
     handleClick(i, category){
         console.info("Client: Asking gamelogic to move player.");
         
-        // IGNORE CLICK IF NOT ON SQUARE FOR POTENTIAL MOVE
+        // IGNORE CLICKS IF NOT ON SQUARE FOR POTENTIAL MOVE
         if (this.game.board.squares[i] !== "O")
             return;
         
@@ -44,8 +65,17 @@ class Client extends React.Component {
         }
         else {
             console.info("Client: The category is - " + question["category"]);
-            console.info("Client: The Question is - " + question["question"]);
-            alert("The category is - " + category + "\nClient: The Question is - " + question["question"]);
+            console.info("Client: The question is - " + question["question"]);
+            console.info("Client: The answers are - " + question["answers"]);
+            
+            if(this.questionPrompt(question) == "correct"){
+                console.info('Client: Player provided answer is CORRECT!')
+                alert("Correct answer!\nRoll for another turn.");
+            }
+            else {
+                console.info('Client: Player provided answer is incorrect :(')
+                this.game.updateCurrentPlayer()
+            }
         }
         this.currentPlayer = this.game.getCurrentPlayer();
         this.jumpTo();
@@ -68,7 +98,12 @@ class Client extends React.Component {
                 <button onClick={() => this.showBoardMove()}>Roll Dice</button>
             </li>
 
-        const configButton =
+        // const initGameButton =
+        //     <li>
+        //         <button onClick={() => this.game.updatePlayers()}>Initialize Game</button>
+        //     </li>
+
+        const configButton = 
             <li>
                 <button onClick={() => this.getConfiguration()}>Configure game</button>
             </li>            
@@ -76,6 +111,11 @@ class Client extends React.Component {
         let displayRoll, displayPlayer;
         displayRoll = "Dice Roll: " + this.diceValue;
         displayPlayer = "Current Player: " + this.currentPlayer;
+        
+
+        var tokenContent = "Player " + this.currentPlayer + 
+                           " token: " + "Red, Blue, White"
+    
 
         this.game.updatePlayers();
 
@@ -90,13 +130,42 @@ class Client extends React.Component {
                 <div className="game-info">
                     <div>{displayRoll}</div>
                     <div>{displayPlayer}</div>
+                    <div>{tokenContent}</div>
                     <ul>
                         {diceRollButton}
                     </ul>
+                    {/*<ul>
+                        {initGameButton}
+                    </ul>*/}
                     <ul>
                         {configButton}
                     </ul>
                 </div>
+                Places Category
+                <div className="board-row">
+                    <PlaceSquare />
+                </div>
+                Events Category
+                <div className="board-row">
+                    <EventSquare />
+                </div>
+                People Category
+                <div className="board-row">
+                    <PeopleSquare />
+                </div>
+                Holiday Category
+                <div className="board-row">
+                    <HolidaySquare />
+                </div>
+                Roll Again
+                <div className="board-row">
+                    <RollagainSquare />
+                </div>
+                Hub Square
+                <div className="board-row">
+                    <HubSquare />
+                </div>
+
             </div>
         );
     }
